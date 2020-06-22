88aabbbbb7aad25e5b367ccf6bee9b39c4162024
everton.almeida <everton.almeida@somospi.com.br>
2020-06-22 13:13:41 -0300

    Alteração no tratamento de exceção do cpf

--- src/main/java/com/schedule/repository/AssociatedRepository.java
+++ src/main/java/com/schedule/repository/AssociatedRepository.java
@@ -1,10 +1,14 @@
 package com.schedule.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.schedule.model.Associated;
 
 public interface AssociatedRepository extends JpaRepository<Associated, Long> {
+	@Query(value ="SELECT v.id FROM public.associated v WHERE v.cpf = :cpf", nativeQuery = true)
+	Integer checkExistence(@Param("cpf") String cpf);
 
 
 }
--- src/main/java/com/schedule/service/AssociatedService.java
+++ src/main/java/com/schedule/service/AssociatedService.java
@@ -14,51 +14,48 @@ import com.schedule.repository.AssociatedRepository;
 
 @Service
 public class AssociatedService {
-	
-	
+
 	@Autowired
 	private AssociatedRepository associatedRepository;
-	
+
 	public void create(Associated associated) {
-		try {
-			associated.setCreationDate(LocalDateTime.now());
-			associatedRepository.saveAndFlush(associated);
-		}catch(Exception e) {
-			throw new BadRequestException("Esse CPF já consta na base de dados.");
+		associated.setCreationDate(LocalDateTime.now());
+		if (associatedRepository.checkExistence(associated.getCpf()) != null) {
+			throw new BadRequestException("Esse cpf já esta cadastrado na base de dados.");
 		}
-		
+		associatedRepository.saveAndFlush(associated);
 	}
-	
-	public List<Associated>getAssociated(){
-		return associatedRepository.findAll();		
+
+	public List<Associated> getAssociated() {
+		return associatedRepository.findAll();
 	}
-	
+
 	public Associated getById(Long id) throws NotFoundException {
 		Optional<Associated> optionalAssociated = getByIdOptional(id);
 		Associated associated = null;
-		if(!optionalAssociated.isPresent()) {
-			throw new NotFoundException("Associado não encontrado para o ID: "+id);
-		}else {
+		if (!optionalAssociated.isPresent()) {
+			throw new NotFoundException("Associado não encontrado para o ID: " + id);
+		} else {
 			associated = optionalAssociated.get();
 		}
 		return associated;
 	}
-	
+
 	public void deleteById(Long id) throws NotFoundException {
 		Optional<Associated> optionalAssociated = getByIdOptional(id);
-		if(!optionalAssociated.isPresent()) {
-			throw new NotFoundException("Associado não encontrado para o ID: "+id);
-		}else {
+		if (!optionalAssociated.isPresent()) {
+			throw new NotFoundException("Associado não encontrado para o ID: " + id);
+		} else {
 			associatedRepository.delete(optionalAssociated.get());
-		}			
+		}
 	}
-	
-	private Optional<Associated> getByIdOptional(Long id){
+
+	private Optional<Associated> getByIdOptional(Long id) {
 		Optional<Associated> optionalAssociated = associatedRepository.findById(id);
 		return optionalAssociated;
 	}
 
-	public void update(Associated newAssociated,Long id) {
+	public void update(Associated newAssociated, Long id) {
 		Associated associatedOld = new Associated();
 		associatedOld = getById(id);
 		newAssociated.setId(associatedOld.getId());
